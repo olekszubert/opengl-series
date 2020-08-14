8ef96011140e627e21d66a1510c46b0e1a315e62
Olek Szubert <olekszubert@gmail.com>
2020-08-14 15:27:21 +0200

    Added textures (for rectangle). Textures loaded by stb_image lib

---
+++ LearnOpenGL/src/paths.h
@@ -0,0 +1,8 @@
+#pragma once
+
+//Shader ourShader(vertexShaderPath, fragShaderPath);
+const char* vertexShaderPath = "C:/dev/LearnOpenGL/LearnOpenGL/src/3.3.shader.vs";
+const char* fragShaderPath = "C:/dev/LearnOpenGL/LearnOpenGL/src/3.3.shader.fs";
+
+const char* containerTexturePath = "C:/dev/LearnOpenGL/resources/textures/container.jpg";
+const char* faceTexturePath = "C:/dev/LearnOpenGL/resources/textures/awesomeface.png";
