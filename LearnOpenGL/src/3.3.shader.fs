815014349f68530d7c440b7533f2175ee22c91f0
Olek Szubert <olekszubert@gmail.com>
2020-08-17 15:33:52 +0200

    Created a rotating cube

--- LearnOpenGL/src/3.3.shader.fs
+++ LearnOpenGL/src/3.3.shader.fs
@@ -1,7 +1,8 @@
+
+
 #version 330 core
 out vec4 FragColor;
 
-in vec3 ourColor;
 in vec2 TexCoord;
 
 // texture samplers
@@ -13,3 +14,4 @@ void main()
 	// linearly interpolate between both textures (80% container, 20% awesomeface)
 	FragColor = mix(texture(texture1, TexCoord), texture(texture2, TexCoord), 0.2);
 }
+
